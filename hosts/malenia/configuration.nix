a3b8b6d938f0cff50dc0e7de6bb46018390d1981
Free-Rat <lawicki02@gmail.com>
2024-11-25 22:41:28 +0100

    cleaning hosts configs

--- hosts/malenia/configuration.nix
+++ hosts/malenia/configuration.nix
@@ -1,24 +1,19 @@
-{ config, pkgs, ... }:
-{
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+{ pkgs, ... } : {
+  imports = [ ./hardware-configuration.nix ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "malenia"; # Define your hostname.
+  networking.hostName = "malenia";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   networking.networkmanager.enable = true;
 
   hardware.graphics = { 
 		enable = true;
-		# enalbe32
   };
 
   time.hardwareClockInLocalTime = true;
@@ -37,7 +32,7 @@
   };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # services.printing.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.freerat = {
@@ -51,6 +46,7 @@
     ];
     packages = with pkgs; [
       firefox
+	  vim
     ];
   };
 
@@ -60,7 +56,5 @@
     options = "--delete-older-than 7d";
   };
 
-#  hardware.keyboard.qmk.enable = true;
-
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 }
