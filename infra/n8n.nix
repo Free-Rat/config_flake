61817ba577d3f5b09ba12cd1d027025705820458
Free-Rat <lawicki02@gmail.com>
2026-07-20 22:57:05 +0200

    n8n runners

--- infra/n8n.nix
+++ infra/n8n.nix
@@ -9,6 +9,12 @@
       N8N_PROTOCOL = "http";
       WEBHOOK_URL = "https://n8n.free-rat.dev";
       N8N_EDITOR_BASE_URL = "https://n8n.free-rat.dev";
+      N8N_CODE_ENABLE_ALL_MODULES = "true";
+      N8N_RUNNERS_AUTH_TOKEN_FILE = "/var/lib/n8n-task-runners/auth-token";
+    };
+
+    taskRunners = {
+      enable = true;
     };
   };
 
